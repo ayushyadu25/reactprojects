93e881c732084bd02c61029b2d326a24a0baabf1
Ayush Yaduvanshi <yaduvanshia422@gmail.com>
2026-02-11 11:31:47 +0530

    redux

--- ImageReduxProject/src/pages/CollectionPages.jsx
+++ ImageReduxProject/src/pages/CollectionPages.jsx
@@ -8,30 +8,30 @@ const CollectionPages = () => {
 
   const collection = useSelector(state => state.collection.items)
   const dispatch = useDispatch()
-  const clearAll =()=>{
+  const clearAll = () => {
     dispatch(clearCollection())
   }
 
   return (
 
-    <div className="flex flex-col ">
+    <div className="flex flex-col bg-gray-900 min-h-screen">
       <div className="flex  gap-2 bg-gray-900">
-        <button className="text-2xl p-2 m-5 ml-10 bg-blue-900 active:scale-95 w-28 rounded text-center"><Link  to='/' >Back</Link></button>
-        <button 
-        onClick={()=>{
-          clearAll()
-        }}
-        className="text-2xl p-2 m-5 bg-red-900 active:scale-95 w-48 rounded text-center ml-280">Clear Collection</button>
-        </div>
+        <button className="text-2xl p-2 m-5 ml-10 bg-blue-900 active:scale-95 w-28 rounded text-center"><Link to='/' >Back</Link></button>
+        <button
+          onClick={() => {
+            clearAll()
+          }}
+          className="text-2xl p-2 m-5 bg-red-900 active:scale-95 w-48 rounded text-center ml-280">Clear Collection</button>
+      </div>
 
-      <div className="flex justify-start w-full flex-wrap gap-6 overflow-auto px-10 bg-gray-900 min-h-screen">
-        {collection.length>0?
+      <div className="flex justify-start w-full flex-wrap gap-6 overflow-auto px-10 bg-gray-900 ">
+        {collection.length > 0 ?
           collection.map((items, idx) => {
             return <div key={idx}>
 
               <CollectionsCard items={items} />
             </div>
-          }):<div className="text-4xl p-20 m-20 text-center pl-[430px]" >Your Collection is Empty</div>
+          }) : <div className="text-4xl p-20 m-20 text-center pl-[430px]" >Your Collection is Empty</div>
         }
       </div>
     </div>
